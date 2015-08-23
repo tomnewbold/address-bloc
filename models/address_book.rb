5102c2cba468922f7d0e843e005daca821d1f0d1
Tommy Newbold <tommynewbold@googlemail.com>
2015-08-23 11:46:34 +0100

    Checkpoint 22 completed, Search

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,6 +19,24 @@ def initialize
     @entries.insert(index, Entry.new(name, phone, email))
   end
 
+#Search AddressBook for a specific entry by name
+  def binary_search(name)
+    lower = 0
+    upper = entries.length -1
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+    return nil
+  end
+
   def import_from_csv(file_name)
 
     csv_text = File.read(file_name)
