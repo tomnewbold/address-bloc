44fdbf0d6d207478864226ff8f2af08ee9f96fec
Tommy Newbold <tommynewbold@googlemail.com>
2015-08-24 19:50:52 +0100

    Assignment 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -37,6 +37,15 @@ def initialize
     return nil
   end
 
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    return nil
+  end
+
   def import_from_csv(file_name)
 
     csv_text = File.read(file_name)
